446c3ca7fc945f2ff6e5caae290b7a9c6305c7cc
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2024-02-09 13:45:42 +0700

    lab 17.2 Complete

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&x, int N){
+    x = new char[N];    
+	for(int i = 0; i < N;i++) x[i] = 'A'+i;
+	x[N] = 0;
 }
 
 int main(){
